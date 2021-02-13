2d214d657054dc60cf3a5b93aefd4c9e77a6c4b9
WejdanCS <wejdanaljadani20@gmail.com>
2021-02-13 05:13:52 +0300

    add sass files

--- src/server/mockAPI.js
+++ src/server/mockAPI.js
@@ -1,7 +1,14 @@
-let json = {
-    'title': 'test json response',
-    'message': 'this is a message',
-    'time': 'now'
+// let json = {
+//     'title': 'test json response',
+//     'message': 'this is a message',
+//     'time': 'now'
+// }
+// https://api.meaningcloud.com/sentiment-2.1
+const dotenv = require('dotenv');
+dotenv.config();
+var apikey = {
+    apiKey: process.env.API_KEY,
 }
+console.log(`Your API key is ${process.env.API_KEY}`);
 
-module.exports = json
+module.exports = apikey
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,24 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+    // const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    // output: {
+    //     libraryTarget: 'var',
+    //     library: 'Client'
+    // },
     mode: 'production',
     module: {
-        rules: [
-            {
+        rules: [{
                 test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
             }
         ]
     },
@@ -18,6 +26,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        // new MiniCssExtractPlugin({ filename: '[name].css' }),
     ]
 }
