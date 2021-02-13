9cf536778349755cb87a39b327152f005a9e3b25
WejdanCS <wejdanaljadani20@gmail.com>
2021-02-14 02:28:05 +0300

    setup server and get data from Meaning API

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,10 +1,32 @@
 var validUrl = require('valid-url');
 
-// check if url is valid url or not 
-function checkArticleUrl(inputText) {
+const postArticleUrl = async function(url = '', data = {}) {
+        console.log(url);
+        console.log(data)
+        const result = await fetch(url, {
+            method: 'POST',
+            // mode: 'cors',
+            credentials: 'same-origin',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(data),
+        });
+        try {
+            return await result.json();
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    // check if url is valid url or not 
+async function checkArticleUrl(articleUrl) {
     // console.log("::: Running Url Checker :::", inputText);
-    if (validUrl.isUri(inputText)) {
+    if (validUrl.isUri(articleUrl)) {
         console.log('Looks like an URI');
+        const data = await postArticleUrl("localhost:8081/", {
+            articleUrl
+        });
+        console.log("data:");
+        console.log(data);
+
 
     } else {
         alert("please enter valid url")
--- src/server/index.js
+++ src/server/index.js
@@ -1,17 +1,24 @@
 var path = require('path')
 const express = require('express')
+const cors = require("cors");
+const bodyParser = require("body-parser")
 const mockAPIResponse = require('./mockAPI.js')
-
+const fetch = require('node-fetch');
 const app = express()
+app.use(cors())
+app.use(bodyParser.json())
+    // to use url encoded values
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.use(express.static('dist'))
+    // console.log(__dirname)
 
-console.log(__dirname)
-
-app.get('/', function(req, res) {
-    // res.sendFile('dist/index.html')
-    res.sendFile(path.resolve('src/client/views/index.html'))
-})
+// app.get('/', function(req, res) {
+//     // res.sendFile('dist/index.html')
+//     res.sendFile(path.resolve('src/client/views/index.html'))
+// })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8081, function() {
@@ -22,11 +29,38 @@ app.get('/test', function(req, res) {
     res.send(mockAPIResponse)
 })
 
-var bas_url = "https://api.meaningcloud.com/sentiment-2.1?key=";
-var url = "https://www.bbc.com/news/world-us-canada-56035306";
-var getSematic = async(req, res) => {
-    console.log(`response: ${mockAPIResponse.apiKey}`)
-    var result = await app.get(bas_url + mockAPIResponse.apiKey + "&of=json&txt=Main" + "lang=eng");
-    console.log(bas_url + mockAPIResponse.apiKey + "&of=json&url=" + url + "&" + "lang=en");
+var baseUrl = "https://api.meaningcloud.com/sentiment-2.1";
+// var articleUrl = "https://www.bbc.com/news/world-us-canada-56035306";
+// var getSematic = async(req, res) => {
+//     console.log(`response: ${mockAPIResponse.apiKey}`)
+//     var result = await app.get(baseUrl + mockAPIResponse.apiKey + "&of=json&txt=Main" + "lang=eng");
+//     console.log(baseUrl + mockAPIResponse.apiKey + "&of=json&url=" + articleUrl + "&" + "lang=en");
 
-}
+// }
+
+app.post('/', async function(req, res) {
+        var articleUrl = req.body.articleUrl;
+        console.log(req.body.articleUrl);
+        console.log(`${baseUrl}?key=${mockAPIResponse.apiKey}&of=json&url=${articleUrl}&lang=en`);
+        const result = await fetch(`${baseUrl}?key=${mockAPIResponse.apiKey}&of=json&url=${articleUrl}&lang=en`);
+        try {
+            // console.log(result.json())
+            data = await result.json();
+            console.log("data")
+            console.log(data)
+                /**
+                 * 
+                 *"score_tag": "P",
+                 "agreement": "DISAGREEMENT",
+                 "subjectivity": "SUBJECTIVE",
+                 "confidence": "83",
+                 "irony": "NONIRONIC",
+                 */
+            res.json(data);
+
+        } catch (error) {
+            console.log(`Error:${error.message}`);
+            res.send(error.message);
+        }
+    })
+    // app.post('/PostArticleUrl',)
